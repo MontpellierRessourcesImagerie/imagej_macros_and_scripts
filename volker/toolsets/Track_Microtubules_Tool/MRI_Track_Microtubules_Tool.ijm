5f65e83f8860cbcf59f394bbd7de03212c6b5325
volker-baecker <volker.baecker@mri.cnrs.fr>
2019-05-10 20:54:52 +0200

    just the tracking part missing now

--- volker/toolsets/Track_Microtubules_Tool/MRI_Track_Microtubules_Tool.ijm
+++ volker/toolsets/Track_Microtubules_Tool/MRI_Track_Microtubules_Tool.ijm
@@ -6,47 +6,47 @@ var _PRECISION_RADIUS = 1;
 
 var helpURL = "https://github.com/MontpellierRessourcesImagerie/imagej_macros_and_scripts/wiki/Track_Microtubules_Tool";
 
-macro "track microtubules tools help [f4]" {
+macro "track microtubules tools help [f1]" {
 	run('URL...', 'url='+helpURL);
 }
 
-macro "track microtubules tools help Action Tool (f4) - C111D01D02D05D06D07D08D0cD0dD0eD0fD1cD1dD1fD28D29D2aD2bD2cD2dD2fD35D38D39D3aD3bD3cD3dD41D46D47D48D49D4aD4bD4cD4dD4eD4fD51D5cD5dD5eD5fD60D70D9fDa0Da1Da2Db0Db1Db2Db4Db9DbaDbeDc0Dc1Dc2Dc4Dc5Dc9DcbDccDceDcfDd0Dd1Dd2Dd3Dd4Dd5DddDdeDdfDe0De1De2De3De4DeeDefDf0Df1Df2Df3Df4C222D0aD0bD10D1aD20D27D32D33D52D53D54D58D6cD92D94D97D98D99D9aD9bD9cD9eDd7Dd8Dd9De6De8De9Df6Df7DfeDffC111D00D03D04D09D11D12D13D14D15D16D17D18D19D1bD1eD21D22D23D24D25D26D2eD30D31D34D36D37D3eD3fD40D42D43D44D45D50D55D56D57D59D5aD5bD80D90D91Da3Da4Da5Da6Da7Da8Da9DaaDabDacDadDaeDafDb3Db5Db6Db7Db8DbbDbcDbdDbfDc3Dc6Dc7Dc8DcaDcdDd6DdaDdbDdcDe5DeaDebDecDedDf5Df8Df9DfaDfbDfcDfdC333D66D69D86D87C222D6bD93D95D96De7C737D61D62D63D71D72D73D81D82D83C277D6dD6eD6fD7dD7eD7fD8dD8eD8fC333D6aD84D85C555D8cC444D64C888D74D75D76D77D78D79D7aD7bD7cC444D65D67D68D88D9dC555D89D8aD8b" {
+macro "track microtubules tools help Action Tool (f1) - C111D01D02D05D06D07D08D0cD0dD0eD0fD1cD1dD1fD28D29D2aD2bD2cD2dD2fD35D38D39D3aD3bD3cD3dD41D46D47D48D49D4aD4bD4cD4dD4eD4fD51D5cD5dD5eD5fD60D70D9fDa0Da1Da2Db0Db1Db2Db4Db9DbaDbeDc0Dc1Dc2Dc4Dc5Dc9DcbDccDceDcfDd0Dd1Dd2Dd3Dd4Dd5DddDdeDdfDe0De1De2De3De4DeeDefDf0Df1Df2Df3Df4C222D0aD0bD10D1aD20D27D32D33D52D53D54D58D6cD92D94D97D98D99D9aD9bD9cD9eDd7Dd8Dd9De6De8De9Df6Df7DfeDffC111D00D03D04D09D11D12D13D14D15D16D17D18D19D1bD1eD21D22D23D24D25D26D2eD30D31D34D36D37D3eD3fD40D42D43D44D45D50D55D56D57D59D5aD5bD80D90D91Da3Da4Da5Da6Da7Da8Da9DaaDabDacDadDaeDafDb3Db5Db6Db7Db8DbbDbcDbdDbfDc3Dc6Dc7Dc8DcaDcdDd6DdaDdbDdcDe5DeaDebDecDedDf5Df8Df9DfaDfbDfcDfdC333D66D69D86D87C222D6bD93D95D96De7C737D61D62D63D71D72D73D81D82D83C277D6dD6eD6fD7dD7eD7fD8dD8eD8fC333D6aD84D85C555D8cC444D64C888D74D75D76D77D78D79D7aD7bD7cC444D65D67D68D88D9dC555D89D8aD8b" {
 	run('URL...', 'url='+helpURL);
 }
 
-macro 'stack registration [f1]' {
+macro 'stack registration [f2]' {
 	registerStacks();
 }
 
-macro "stack registration Action Tool (f1) - C000T4b12s" {
+macro "stack registration Action Tool (f2) - C000T4b12s" {
 	registerStacks();	
 }
 
-macro 'find end points [f2]' {
+macro 'find end points [f3]' {
 	skeletonizeMicrotubules();
 	findEndPoints(_CURRENT_IMAGE_ID);
 }
 
 
-macro "find end points Action Tool (f2) - C000T4b12f" {
+macro "find end points Action Tool (f3) - C000T4b12f" {
 	skeletonizeMicrotubules();
 	findEndPoints(_CURRENT_IMAGE_ID);
 }
 
 
-macro "add to selection Action Tool (f3) - C000T4b12a" {
+macro "add to selection Action Tool (f4) - C000T4b12a" {
 	addToSelection();
 }
 
-macro 'add to selection [f3]' {
+macro 'add to selection [f4]' {
 	addToSelection();
 }
 
-macro 'track ends [f4]' {
+macro 'track ends [f5]' {
 	trackEnds();
 }
 
-macro "track ends Action Tool (f4) - C000T4b12t" {
+macro "track ends Action Tool (f5) - C000T4b12t" {
 	trackEnds();
 }
 
--- volker/toolsets/Track_Microtubules_Tool/microtubule-tracking.py
+++ volker/toolsets/Track_Microtubules_Tool/microtubule-tracking.py
@@ -21,7 +21,10 @@ def trackEnds(imageID, maskID):
     points2 = inputIMP.getOverlay().get(1).getContainedPoints()[index]
     roi = maskIMP.getOverlay().get(index)
     outerBounds = roi.getBounds()
-    impMT, innerBounds = duplicateMaskInRoi(maskIMP, roi)
+    impMT, innerBounds = duplicateMaskInRoi(maskIMP, roi, True)
+    centroid = impMT.getOverlay().get(0).getContourCentroid()
+    nr, endPoint1, endPoint2 = findEndPointsInSkeleton(impMT)
+    print(nr, endPoint1, endPoint2)
     impMT.show()
         
 def findEndPoints(imageID, maskID):
@@ -38,20 +41,8 @@ def findEndPoints(imageID, maskID):
     for roi in rois:
        outerBounds = roi.getBounds()
        impMT, innerBounds = duplicateMaskInRoi(imp, roi)
-       points = getPointsFromSekelton(impMT)
-       nr = 0
-       endPoint1 = (-1,-1)
-       endPoint2 = (-1,-1)
-       for point in points:
-           rect = Roi(point.x-1, point.y-1, 3, 3)
-           impMT.setRoi(rect)
-           stats = impMT.getStatistics()
-           if (stats.mean > 56 and stats.mean<57):
-               if (nr==0):
-                   endPoint1 = point
-               else: 
-                   endPoint2 = point
-               nr = nr + 1
+       nr, endPoint1, endPoint2 = findEndPointsInSkeleton(impMT)
+       print(nr, endPoint1, endPoint2)
        if (nr==2):
            endPoint1.x = endPoint1.x + outerBounds.x + innerBounds.x - 1
            endPoint1.y = endPoint1.y + outerBounds.y + innerBounds.y - 1
@@ -73,16 +64,43 @@ def findEndPoints(imageID, maskID):
     otherPoints.setFillColor(Color.cyan)
     inputIMP.getOverlay().add(otherPoints)
     
-	
+def findEndPointsInSkeleton(impMT):
+    points = getPointsFromSekelton(impMT)
+    nr = 0
+    endPoint1 = (-1,-1)
+    endPoint2 = (-1,-1)
+    for point in points:
+        rect = Roi(point.x-1, point.y-1, 3, 3)
+        impMT.setRoi(rect)
+        stats = impMT.getStatistics	()
+        if (stats.mean > 56 and stats.mean<57):
+            if (nr==0):
+                endPoint1 = point
+            else: 
+                endPoint2 = point
+            nr = nr + 1
+    return nr, endPoint1, endPoint2
+               
 # duplicates the mask in the roi with a border of 1 pixel
-def duplicateMaskInRoi(imp, roi):
+def duplicateMaskInRoi(imp, roi, duplicateStack=False):
     imp.setRoi(roi)
-    impMT1 = imp.crop()
+    if duplicateStack:
+    	WindowManager.setCurrentWindow(imp.getWindow())
+        impMT1 = IJ.run("Duplicate...", "duplicate")
+        impMT1 = WindowManager.getCurrentImage()
+    else:
+        impMT1 = imp.crop()
     IJ.run("Set Scale...", "distance=0 known=0 pixel=1 unit=pixel")
     IJ.run(impMT1, "Create Selection", "")
     roi1 = impMT1.getRoi()
     bounds = roi1.getBounds()
-    impMT = impMT1.crop()
+    if duplicateStack:
+       	WindowManager.setCurrentWindow(impMT1.getWindow())
+        IJ.run("Duplicate...", "duplicate")
+        impMT = WindowManager.getCurrentImage()
+        impMT1.close()
+    else:
+        impMT = impMT1.crop()
     IJ.run(impMT, "Canvas Size...", "width="+str(bounds.width+2)+" height="+str(bounds.height+2)+" position=Center zero")  
     return impMT, bounds
 
