53071bbdce51a38955623719eb5c62fde6405099
volker-baecker <volker.baecker@mri.cnrs.fr>
2022-03-22 18:00:04 +0100

    renamed

--- volker/toolsets/radial_movement_analyzer/radial_movement_analyzer.py
+++ volker/toolsets/radial_movement_analyzer/radial_movement_analyzer.py
@@ -1,5 +1,5 @@
 '''
- Moving particles divergence tool. 
+ Radial movement analyzer. 
 
 (c) 2022, INSERM
 written by Volker Baecker at Montpellier Ressources Imagerie, Biocampus Montpellier, INSERM, CNRS, University of Montpellier (www.mri.cnrs.fr)
@@ -10,6 +10,7 @@ displacement of each particle from one timepoint t to the next t+1, onto the vec
 If the particles are moving away from the given point the result will be positive, if they are moving towards the given point the result will be negative.
 If neither of the above is the case, the result will be near zero.
 '''
+from __future__ import division
 from ij.measure import ResultsTable
 from ij.gui import Plot
 from ij.util import Tools
@@ -18,6 +19,8 @@ from ij.gui import Roi
 from ij.gui import NewImage
 import jarray
 import math 
+
+TABLE_NAME = "Radial Velocity"
 
 def main(tableName, showPlot):
     image = IJ.getImage();
@@ -28,13 +31,14 @@ def main(tableName, showPlot):
         center = roi.getXBase(), roi.getYBase();
     table = ResultsTable.getResultsTable(tableName)
     vectors = getVectorsFromTable(table, center)
-    divergence = calculateDivergencePerTime(vectors, center)
-    stats = Tools.getStatistics(divergence)
-    median = calculateMedian(divergence)
-    rt = ResultsTable.getResultsTable ("Divergence")
+    radialVelocity = calculateRadialVelocityPerTime(vectors, center)
+    stats = Tools.getStatistics(radialVelocity)
+    median = calculateMedian(radialVelocity)
+    rt = ResultsTable.getResultsTable(TABLE_NAME)
     if not rt:
         rt = ResultsTable()
     row = rt.getCounter()
+    print("row", row)
     rt.setValue("label", row, tableName)
     rt.setValue("x", row, center[0])
     rt.setValue("y", row, center[1])
@@ -43,32 +47,33 @@ def main(tableName, showPlot):
     rt.setValue("min", row, stats.min)
     rt.setValue("median", row, median)
     rt.setValue("max", row, stats.max)
-    rt.show("Divergence")
+    rt.show(TABLE_NAME)
     if showPlot:
-        plot(divergence, center)
+        plot(radialVelocity, center)
     
 def plot(data, center):
     T = list(range(0, len(data)))
-    plot = Plot("divergence in (" + str(center[0]) + ", "+ str(center[1])+")", "time[1]", "divergence", T, data)
+    plot = Plot("radial velocity in (" + str(center[0]) + ", "+ str(center[1])+")", "time[1]", "radial velocity", T, data)
     plot.show()
     
-def calculateDivergencePerTime(vectors, aPoint):
-    divergences = []
+def calculateRadialVelocityPerTime(vectors, aPoint):
+    velocities = []
     for row in vectors:
         newRow = []
         for vector in row:
-            newRow.append(proj(vector[1], (vector[0][0]-aPoint[0], vector[0][1]-aPoint[1])))
-        divergences.append(sum(newRow)/len(newRow))
-    return divergences
+            magnitude = math.sqrt(vector[1][0]**2 + vector[1][1]**2)
+            newRow.append(proj(vector[1], (vector[0][0]-aPoint[0], vector[0][1]-aPoint[1]))/magnitude)
+        velocities.append(sum(newRow)/len(newRow))
+    return velocities
 
-def divergenceMapAt(vectors, aTime, width, height):
+def radialVelocityMapAt(vectors, aTime, width, height):
     vectorsAtTime = vectors[aTime:aTime+1]
     dMap = []
     for x in range(0, width):
-        print("divergence map processing row " + str(x+1) + " of " + str(width))
+        print("radial velocity map processing row " + str(x+1) + " of " + str(width))
         for y in range(0, height):
-            dMap.append(calculateDivergencePerTime(vectors, (x,y))[0])
-    image = NewImage.createFloatImage ("divergence map from t=" + str(aTime) + " to t=" + str(aTime), width, height, 1, NewImage.FILL_BLACK)
+            dMap.append(calculateRadialVelocityPerTime(vectors, (x,y))[0])
+    image = NewImage.createFloatImage ("radial velocity map from t=" + str(aTime) + " to t=" + str(aTime), width, height, 1, NewImage.FILL_BLACK)
     image.getProcessor().setPixels(jarray.array(dMap, 'f'))
     image.updateAndDraw()
     return image
@@ -114,7 +119,8 @@ def getVectorsFromTable(table, center):
             dx = x2 - x1
             dy = y2 - y1
             row.append(((x1, y1), (dx, dy)))
-        vectors.append(row)       
+        vectors.append(row)      
+    print(vectors) 
     return vectors
     
 if 'getArgument' in globals():
