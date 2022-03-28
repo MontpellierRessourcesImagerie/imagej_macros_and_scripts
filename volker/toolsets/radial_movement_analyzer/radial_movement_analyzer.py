0bf39c0bc3686289f47d7e283753ad10501129b3
volker-baecker <volker.baecker@mri.cnrs.fr>
2022-03-28 18:01:07 +0200

    report delta distance with trackID, ok

--- volker/toolsets/radial_movement_analyzer/radial_movement_analyzer.py
+++ volker/toolsets/radial_movement_analyzer/radial_movement_analyzer.py
@@ -1,16 +1,6 @@
-'''
- Radial movement analyzer. 
-
-(c) 2022, INSERM
-written by Volker Baecker at Montpellier Ressources Imagerie, Biocampus Montpellier, INSERM, CNRS, University of Montpellier (www.mri.cnrs.fr)
-
-The tool takes a results table of tracking data (as created by Trackmate) and calculates the mean of the scalar projections of the vector of the 
-displacement of each particle from one timepoint t to the next t+1, onto the vector from a given point to the coordinates of the particle at timepoint t 
-
-If the particles are moving away from the given point the result will be positive, if they are moving towards the given point the result will be negative.
-If neither of the above is the case, the result will be near zero.
-'''
 from __future__ import division
+DEPLOYED = True;
+from operator import sub
 from ij.measure import ResultsTable
 from ij.gui import Plot
 from ij.util import Tools
@@ -20,7 +10,10 @@ from ij.gui import NewImage
 import jarray
 import math 
 
-TABLE_NAME = "Radial Velocity"
+TABLE_NAME = "Distance from center"
+
+if not DEPLOYED:
+    import unittest, sys
 
 def main(tableName, showPlot):
     image = IJ.getImage();
@@ -30,142 +23,230 @@ def main(tableName, showPlot):
     else:
         center = roi.getXBase(), roi.getYBase();
     table = ResultsTable.getResultsTable(tableName)
-    vectors = getVectorsFromTable(table, center)
-    radialVelocity = calculateRadialVelocityPerTime(vectors, center)
-    radialVelocityAndDistanceByTrack(table, center)
-    stats = Tools.getStatistics(radialVelocity)
-    median = calculateMedian(radialVelocity)
+    rma = RadialMovementAnalyzer(table, center)
+    distances = rma.getDeltaDistancePerTrack()
     rt = ResultsTable.getResultsTable(TABLE_NAME)
     if not rt:
         rt = ResultsTable()
-    row = rt.getCounter()
-    rt.setValue("label", row, tableName)
-    rt.setValue("x", row, center[0])
-    rt.setValue("y", row, center[1])
-    rt.setValue("mean", row, stats.mean)
-    rt.setValue("stdDev", row, stats.stdDev)
-    rt.setValue("min", row, stats.min)
-    rt.setValue("median", row, median)
-    rt.setValue("max", row, stats.max)
+    for index, dist in enumerate(distances):
+        row = rt.getCounter()
+        rt.setValue("label", row, tableName)
+        rt.setValue("track ID", row, rma.trackIDs[index])
+        rt.setValue("total augmentation of distance from center", row, dist)
     rt.show(TABLE_NAME)
     if showPlot:
-        plot(radialVelocity, center)
-    
+        plot(distances, center)
+
 def plot(data, center):
     T = list(range(0, len(data)))
-    plot = Plot("radial velocity in (" + str(center[0]) + ", "+ str(center[1])+")", "time[1]", "radial velocity", T, data)
+    plot = Plot("augmentation of distance from (" + str(center[0]) + ", "+ str(center[1])+")", "track nr", "distance", T, data)
     plot.show()
     
-def calculateRadialVelocityPerTime(vectors, aPoint):
-    velocities = []
-    for row in vectors:
-        newRow = []
-        for vector in row:
-            magnitude = math.sqrt(vector[1][0]**2 + vector[1][1]**2)
-            newRow.append(proj(vector[1], (vector[0][0]-aPoint[0], vector[0][1]-aPoint[1]))/magnitude)
-        velocities.append(sum(newRow)/len(newRow))
-    return velocities
+class Vectors:
+    @classmethod
+    def proj(cls, a, b):
+        f = Vectors.dot(a, b) / Vectors.dot(b, b) 
+        projAonB = [bi*f for bi in b]
+        sign = -1
+        if math.copysign(1, b[0]) == math.copysign(1, projAonB[0]):
+            sign = 1
+        scalarProj = sign * math.sqrt(projAonB[0]**2 + projAonB[1]**2)
+        return scalarProj
 
-def radialVelocityAndDistanceByTrack(table, center):
-    vectors = []
-    headings = list(table.getHeadings())
-    tColumn, pIDColumn, xColumn, yColumn = table.getColumn(headings.index('T')),  \
-                                           table.getColumn(headings.index('TRACK_ID')), \
-                                           table.getColumn(headings.index('X')), \
-                                           table.getColumn(headings.index('Y'))    
-    trackData = []
-    T = {}
-    for t, pID, x, y in zip(tColumn, pIDColumn, xColumn, yColumn):
-        T[(t, pID)] = (x, y)
-    for t, pid in T.keys():
-        if t == tColumn[len(tColumn)-1]: 
-            continue
-        row = []
-            
-        x1 = T[(t, pid)][0] 
-        y1 = T[(t, pid)][1] 
-#          print(pid, t, x1, y1)
-        x2 = T[(t+1, pid)][0] 
-        y2 = T[(t+1, pid)][1] 
-        dx = x2 - x1
-        dy = y2 - y1
-        row.append((pid, (x1, y1), (dx, dy)))
-        vectors.append(row)
-    print(vectors)
-                                               
+    @classmethod
+    def dot(cls, a, b):
+        return sum(x*y for x, y in zip(a, b))
 
-def radialVelocityMapAt(vectors, aTime, width, height):
-    vectorsAtTime = vectors[aTime:aTime+1]
-    dMap = []
-    for x in range(0, width):
-        print("radial velocity map processing row " + str(x+1) + " of " + str(width))
-        for y in range(0, height):
-            dMap.append(calculateRadialVelocityPerTime(vectors, (x,y))[0])
-    image = NewImage.createFloatImage ("radial velocity map from t=" + str(aTime) + " to t=" + str(aTime), width, height, 1, NewImage.FILL_BLACK)
-    image.getProcessor().setPixels(jarray.array(dMap, 'f'))
-    image.updateAndDraw()
-    return image
+    @classmethod
+    def sub(cls, a, b):
+        return tuple(map(sub, a, b))
 
-def proj(a, b):
-    f = dot(a, b) / dot(b, b)
-    projAonB = [bi*f for bi in b]
-    sign = -1
-    if math.copysign(1, b[0]) == math.copysign(1, projAonB[0]):
-        sign = 1
-    scalarProj = sign * math.sqrt(projAonB[0]**2 + projAonB[1]**2)
-    return scalarProj
-    
-def dot(a, b):
-    return sum(x*y for x, y in zip(a, b))
-
-def calculateMedian(aList):
-    ranks = Tools.rank(aList)
-    if len(ranks) % 2 == 0:
-        higherIndex = int(len(ranks)/2) 
-        lowerIndex = higherIndex-1
-        return (aList[lowerIndex] + aList[higherIndex]) / 2.0
-    else:
-        return aList[ranks[int(len(ranks)/2)]]
+    @classmethod
+    def dist(cls, a, b):
+        return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
         
-def getVectorsFromTable(table, center):
-    vectors = []
-    headings = list(table.getHeadings())
-    tColumn, pIDColumn, xColumn, yColumn = table.getColumn(headings.index('T')),  \
-                                           table.getColumn(headings.index('TRACK_ID')), \
-                                           table.getColumn(headings.index('X')), \
-                                           table.getColumn(headings.index('Y'))
-    T = {}
-    for t, pID, x, y in zip(tColumn, pIDColumn, xColumn, yColumn):
-        if not pID in T:
-            t[pID] = []
-        T[pID].append(x, y)
-    for t, pid in T.keys():
-        if t == tColumn[len(tColumn)-1]: 
-            continue
-        row = []
-            
-        x1 = T[(t, pid)][0] 
-        y1 = T[(t, pid)][1] 
-#          print(pid, t, x1, y1)
-        x2 = T[(t+1, pid)][0] 
-        y2 = T[(t+1, pid)][1] 
-        dx = x2 - x1
-        dy = y2 - y1
-        row.append(((x1, y1), (dx, dy)))
-        vectors.append(row)   
-    return vectors
+class RadialMovementAnalyzer:
+
+    timeColumnHeader = 'T'
+    trackColumnHeader = 'TRACK_ID'
+    xColumnHeader = 'X'
+    yColumnHeader = 'Y'
+
+    def __init__(self, table, center):
+        self.table = table
+        self.center = center
+        self.tracks, self.timepoints, self.trackIDs = self.getTrackData()
+        
+    def getTableData(self):
+        vectors = []
+        headings = list(self.table.getHeadings())
+        tColumn = self.table.getColumn(self.timeColumnHeader)
+        pIDColumn = self.table.getColumn(self.trackColumnHeader)
+        xColumn = self.table.getColumn(self.xColumnHeader)
+        yColumn = self.table.getColumn(self.yColumnHeader)    
+        return tColumn, pIDColumn, xColumn, yColumn
+
+    def getTrackData(self):
+        tColumn, pIDColumn, xColumn, yColumn = self.getTableData()
+        timepoints = list(set(tColumn))
+        trackIDs = list(set(pIDColumn))
+        timepoints.sort()
+        trackIDs.sort()
+        T = [[0 for i in xrange(len(timepoints))] for j in xrange(len(trackIDs))]
+        for timepoint, trackID, x, y in zip(tColumn, pIDColumn, xColumn, yColumn):
+            timepointIndex = timepoints.index(timepoint)
+            trackIndex = trackIDs.index(trackID)
+            T[trackIndex][timepointIndex] = (x,y)
+        return T, timepoints, trackIDs
+
+    def getMeanRadialVelocityPerTrack(self):
+        pass
     
-if 'getArgument' in globals():
-    if not hasattr(zip, '__call__'):
-        del zip                     # the python function zip got overriden by java.util.zip, so it must be deleted to get the zip-function to work.
-    parameter = getArgument()
-    args = parameter.split(",")
-    table = args[0].split("=")[1]
-    showPlotParam = args[1].split("=")[1]
-    showPlot = False
-    if showPlotParam=="true":
+    def getDeltaDistancePerTrack(self):
+        distances = []
+        for track in self.tracks:
+            activeTrack = [xoords for xoords in track if not xoords == 0]
+            print(activeTrack)
+            x1, y1 = activeTrack[0]
+            x2, y2 = activeTrack[-1]
+            distStart = Vectors.dist((x1, y1), self.center)
+            distEnd = Vectors.dist((x2, y2), self.center)
+            dist = distEnd - distStart
+            distances.append(dist)
+        return distances
+            
+if DEPLOYED:
+    if 'getArgument' in globals():
+        if not hasattr(zip, '__call__'):
+            del zip                     # the python function zip got overriden by java.util.zip, so it must be deleted to get the zip-function to work.
+        parameter = getArgument()
+        args = parameter.split(",")
+        table = args[0].split("=")[1]
+        showPlotParam = args[1].split("=")[1]
+        showPlot = False
+        if showPlotParam=="true":
+            showPlot = True
+    else:
+        table = "Tracks"
         showPlot = True
-else:
-    table = "Tracks"
-    showPlot = True
-main(table ,showPlot)
+    main(table ,showPlot)
+else:        
+    ###################################################
+    # UNIT TESTS
+    ###################################################
+    class VectorsTest(unittest.TestCase):
+        
+        def testDot(self):
+            res = Vectors.dot((1, 2, 3), (2, 3, 4))
+            self.assertEquals(20, res)
+    
+        def testProj(self):
+            res = Vectors.proj((1, 0), (1, 0))
+            self.assertEquals(1, res)
+            res = Vectors.proj((1, 1), (1, 1))
+            self.assertEquals(math.sqrt(2), res)
+            
+    class RadialMovementAnalyzerTest(unittest.TestCase):
+        table = None
+    
+        def setUp(self):
+            unittest.TestCase.setUp(self)
+            rt = ResultsTable()
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID00002')
+            rt.addValue('ID', 2)
+            rt.addValue('TRACK_ID', 2)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 738.9)
+            rt.addValue('Y', 670.0)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 0)
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID00003')
+            rt.addValue('ID', 3)
+            rt.addValue('TRACK_ID', 3)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 672.1)
+            rt.addValue('Y', 729.3)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 0)
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID00001')
+            rt.addValue('ID', 31)
+            rt.addValue('TRACK_ID', 1)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 953.2)
+            rt.addValue('Y', 803.5)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 1)
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID000032')
+            rt.addValue('ID', 32)
+            rt.addValue('TRACK_ID', 2)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 739.5)
+            rt.addValue('Y', 665.0)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 1)
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID000033')
+            rt.addValue('ID', 33)
+            rt.addValue('TRACK_ID', 3)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 667.0)
+            rt.addValue('Y', 729.8)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 1)
+            rt.incrementCounter()
+            rt.addLabel('LABEL', 'ID000061')
+            rt.addValue('ID', 61)
+            rt.addValue('TRACK_ID', 1)
+            rt.addValue('QUALITY', 1)
+            rt.addValue('X', 959.0)
+            rt.addValue('Y', 805.5)
+            rt.addValue('Z', 0)
+            rt.addValue('T', 2)
+            self.table = rt
+            
+        def testConstructor(self):
+            rma = RadialMovementAnalyzer(self.table, (800,800))
+            self.assertEquals(self.table, rma.table)
+    
+        def testGetTableData(self):
+            rma = RadialMovementAnalyzer(self.table, (800,800))
+            tColumn, pIDColumn, xColumn, yColumn = rma.getTableData()
+            self.assertEquals(0, tColumn[0])
+            self.assertEquals(2, tColumn[5])
+            self.assertEquals(2, pIDColumn[0])
+            self.assertEquals(1, pIDColumn[5])
+    
+        def testGetTrackData(self):
+            rma = RadialMovementAnalyzer(self.table, (800,800))
+            data, _, _ = rma.getTrackData()
+            print(data)
+            self.assertEquals(3, len(data))
+            self.assertEquals(3, len(data[0]))
+            self.assertEquals(3, len(data[1]))
+            self.assertEquals(3, len(data[2]))
+            self.assertEquals(0, data[0][0]) 
+            self.assertEquals(0, data[1][2])
+            self.assertEquals(0, data[2][2])
+    
+        def testGetDeltaDistancePerTrack(self):
+            rma = RadialMovementAnalyzer(self.table, (800,800))
+            distances = rma.getDeltaDistancePerTrack()
+            print(distances)
+            self.assertEquals(3, len(distances))
+            
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(VectorsTest('testDot'))
+        suite.addTest(VectorsTest('testProj'))
+        suite.addTest(RadialMovementAnalyzerTest('testConstructor'))
+        suite.addTest(RadialMovementAnalyzerTest('testGetTableData'))
+        suite.addTest(RadialMovementAnalyzerTest('testGetTrackData'))
+        suite.addTest(RadialMovementAnalyzerTest('testGetDeltaDistancePerTrack'))
+        return suite
+        
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=1)
+    runner.run(suite())
