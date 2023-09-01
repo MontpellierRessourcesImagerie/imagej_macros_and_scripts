f76c7fe2044d5d8ecc27fa2388a990015569e784
volker-baecker <volker.baecker@mri.cnrs.fr>
2023-09-01 16:50:19 +0200

    python updater / download tag instead parsing web-page

--- volker/scripts/mri-updater.py
+++ volker/scripts/mri-updater.py
@@ -1,10 +1,10 @@
 import os
+import shutil
 from urllib import urlopen
 from  zipfile import ZipFile
 from ij import IJ
 from ij import Prefs
 from ij.gui import GenericDialog
-import time 
 
 
 class Updater:
@@ -27,14 +27,67 @@ class Updater:
         self.tagsUrl = repositoryUrl + "tags"
         self.archiveUrl = repositoryUrl + 'archive/refs/tags/'
         self.downloadBaseUrl = repositoryUrl + "/releases/tag/"
-                
+        self.sourceFolder = ""
+        
+        self.pluginsToolDir = IJ.getDirectory("plugins") + self.folder + "/"
+        self.toolsetsDir = IJ.getDirectory("macros") +  "toolsets/"
+        self.pythonModulesDir = IJ.getDirectory("imagej") + "/jars/Lib/fr/cnrs/mri/cialib/"
+        
 
     def runUpdate(self):    
         self.tag = self.getTargetVersionFromUser()
-        self.downloadTagFromGithub(self.tag);
-        
-        IJ.showMessage("Update finished, please restart ImageJ!");    
+        self.downloadTagFromGithub(self.tag)
+        self.installTool()
+        IJ.log("Update finished, please restart ImageJ!")
+        IJ.showMessage("Update finished, please restart ImageJ!")
     
+        
+    def installTool(self):
+        files = [self.sourceFolder + f for f in os.listdir(self.sourceFolder ) if os.path.isfile(self.sourceFolder+f)]
+        for file in files:
+            if self.isToolset(file):                
+                IJ.log("writing toolset " + file + " to " + self.toolsetsDir);
+                shutil.copy(file, self.toolsetsDir)
+                continue
+            if self.isModule(file):     
+                IJ.log("writing python module " + file + " to " + self.pluginsToolDir);
+                shutil.copy(file, self.pythonModulesDir)
+                continue
+            IJ.log("writing file " + file + " to " + self.pluginsToolDir);
+            shutil.copy(file, self.pluginsToolDir)
+            
+                    
+    def isToolset(self, file):
+        result = True
+        content = IJ.openUrlAsString("file://" + file)
+        result = result and file.endswith('.ijm')
+        result = result and 'macro "' in content
+        return result
+        
+        
+    def isMacroCommand(self, file):
+        result = True
+        content = IJ.openUrlAsString("file://" + file)
+        result = result and file.endswith('.ijm')
+        result = result and not 'macro "' in content
+        return result
+    
+    
+    def isScript(self, file):
+        result = True
+        content = IJ.openUrlAsString("file://" + file)
+        result = result and file.endswith('.py')
+        result = result and "def main():" in content
+        return result
+        
+        
+    def isModule(self, file):
+        result = True
+        content = IJ.openUrlAsString("file://" + file)
+        result = result and file.endswith('.py')
+        result = result and not "def main():" in content
+        return result
+        
     
     def downloadTagFromGithub(self, tag):
         downloadUrl = self.archiveUrl + tag + ".zip"
@@ -50,7 +103,7 @@ class Updater:
                 f.write(chunk)
         with ZipFile(outputFilePath, 'r') as zObject:
             zObject.extractall(path=tmpFolder)
-        
+        self.sourceFolder = IJ.getDirectory("temp") + "imagej_macros_and_scripts" + "-" + tag[1:] + "/" + self.author + "/toolsets/" + self.tool + "/"
         
     
     def getTargetVersionFromUser(self):
@@ -85,8 +138,7 @@ class Updater:
                 tagsWithTool.append(tag)
         return tagsWithTool;
     
-    
-    
+        
     def readPreferences(self):
         toolToBeUpdated = Prefs.get("mri.update.tool", "")
         updateFolder = Prefs.get("mri.update.folder", "")
@@ -101,8 +153,7 @@ class Updater:
         if updateModulesFolder:
            self.pythonModulesPath = updateModulesFolder
     
-    
-    
+        
     def getCurrentVersion(self):
         pluginsDir = IJ.getDirectory("plugins")
         pluginsToolDir = pluginsDir + self.folder + "/"
