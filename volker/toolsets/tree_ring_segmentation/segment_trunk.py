ec3ef8fe13f5401338b115daee497775dc16a601
volker-baecker <volker.baecker@mri.cnrs.fr>
2024-09-03 18:07:50 +0200

    segment pith

--- volker/toolsets/tree_ring_segmentation/segment_trunk.py
+++ volker/toolsets/tree_ring_segmentation/segment_trunk.py
@@ -8,6 +8,7 @@ from ij.plugin import Scaler
 from ij.process import ImageConverter
 from ij.plugin.filter import ThresholdToSelection
 from ij.plugin import RoiEnlarger
+from sc.fiji.colourDeconvolution import StainMatrix
 from inra.ijpb.binary import BinaryImages
 from inra.ijpb.morphology import Reconstruction
 from inra.ijpb.morphology.filter import Opening
@@ -15,6 +16,8 @@ from inra.ijpb.morphology.filter import Closing
 from inra.ijpb.morphology import Strel
 from inra.ijpb.binary.conncomp import FloodFillComponentsLabeling
 from inra.ijpb.measure.region2d import BoundingBox
+from inra.ijpb.label.select import LabelSizeFiltering
+from inra.ijpb.label.select import RelationalOperator
 from inra.ijpb.label import LabelImages
 
 
@@ -23,13 +26,18 @@ OPENING_RADIUS = 16
 CLOSING_RADIUS = 8
 SCALE_FACTOR = 8
 INTERPOLATION_INTERVAL = 100
+VECTORS = (0.7372839, 0.63264143, 0.23701741,
+           0.91958255, 0.35537627, 0.16755785,
+           0.69067574, 0.64728355, 0.3224746)
+PITH_MIN_SIZE = 200
+AUTO_THRESHOLDING_METHOD = "Mean"           
 
 
 class TreeRingAnalyzer:
     
     
     def __init__(self, image, sigma=2, openingRadius=16, closingRadius=8, scaleFactor=8, 
-                              strokeWidth=8, interpolationInterval=100):
+                              strokeWidth=8, interpolationInterval=100, thresholdingMethod="Huang"):
         self.image = image
         self.width, self.height, self.nChannels, self.nSlices, self.nFrames = self.image.getDimensions() 
         self.sigma = sigma
@@ -41,7 +49,10 @@ class TreeRingAnalyzer:
         self.closingStrel = Strel.Shape.DISK.fromRadius(self.closingRadius)
         self.interpolationInterval = interpolationInterval
         self.scaledDownMask = None
-    
+        self.vectors = None
+        self.thresholdingMethod = thresholdingMethod
+        self.pithMinSize = 500
+        
     
     def getScaledDownMask(self):
         if not self.scaledDownMask:
@@ -52,11 +63,16 @@ class TreeRingAnalyzer:
     def createScaledDownMask(self):
         image = Duplicator().run(self.image, 1, self.nChannels, 1, self.nSlices, 1, self.nFrames)
         ImageConverter(image).convertToGray8()
-        image = Scaler.resize(image, self.width/self.scaleFactor, self.height/self.scaleFactor, 1, 'bilinear')
-        image.getProcessor().blurGaussian(self.sigma)
-        image.getProcessor().setAutoThreshold("Default")
-        image.setProcessor(image.getProcessor().createMask())
-        self.scaledDownMask = image
+        self.scaledDownMask = self.calculateScaledDownMask(image)
+    
+    
+    def calculateScaledDownMask(self, image):
+        mask = image
+        mask = Scaler.resize(mask, self.width/self.scaleFactor, self.height/self.scaleFactor, 1, 'bilinear')
+        mask.getProcessor().blurGaussian(self.sigma)
+        mask.getProcessor().setAutoThreshold(self.thresholdingMethod)
+        mask.setProcessor(mask.getProcessor().createMask())
+        return mask
     
     
     def segmentTrunk(self):
@@ -89,7 +105,15 @@ class TreeRingAnalyzer:
     
     def segmentPith(self):
         componentsLabeling = FloodFillComponentsLabeling(4, 16)
-        labelsProcessor = componentsLabeling.computeLabels(self.getScaledDownMask().getProcessor())
+        sm = StainMatrix()
+        sm.init("wood", *self.vectors)
+        labelFiltering = LabelSizeFiltering(RelationalOperator.GT, self.pithMinSize)
+        
+        stack = sm.compute(False, True, self.image);
+        imp = ImagePlus(self.image.getTitle() + "-(Colour_2)", stack[1])
+        mask = self.calculateScaledDownMask(imp)        
+        labelsProcessor = componentsLabeling.computeLabels(mask.getProcessor())
+        labelsProcessor = labelFiltering.process(labelsProcessor)
         labels = LabelImages.findAllLabels(labelsProcessor)
         label = self.getLabelClosestToCenter(labelsProcessor, labels)
         ip = LabelImages.binarize(labelsProcessor, label)
@@ -102,7 +126,7 @@ class TreeRingAnalyzer:
         roi.setName("pith - outer border")
         roi.setStrokeWidth(self.strokeWidth)
         self.addRoiToOverlay(roi)
-    
+        
     
     def getLabelClosestToCenter(self, labelsProcessor, labels):
         boxes = BoundingBox.boundingBoxes(labelsProcessor, labels, None)
@@ -122,15 +146,17 @@ class TreeRingAnalyzer:
                 closestIndex = index
             index = index + 1
         return closestIndex
-    
-    
-    
+
+
 image = IJ.getImage()
 image.setOverlay(None)
 analyzer = TreeRingAnalyzer(image, sigma=SIGMA, 
                                    openingRadius=OPENING_RADIUS, 
                                    closingRadius=CLOSING_RADIUS,
                                    scaleFactor=SCALE_FACTOR, 
-                                   interpolationInterval=INTERPOLATION_INTERVAL)
+                                   interpolationInterval=INTERPOLATION_INTERVAL,
+                                   thresholdingMethod=AUTO_THRESHOLDING_METHOD)
+analyzer.vectors = VECTORS   
+analyzer.pithMinSize = PITH_MIN_SIZE
 analyzer.segmentTrunk()
 analyzer.segmentPith()
