ddca68f5e2dedc7a6c919078f132f39ccc3f047d
Clément Benedetti <clement.benedetti@mri.cnrs.fr>
2025-10-01 13:36:47 +0200

    fix macro filter spots

--- clement/macros/exclusive-co-occs/filter-slide-spots.py
+++ clement/macros/exclusive-co-occs/filter-slide-spots.py
@@ -1,37 +1,47 @@
 import math
 from ij import IJ
 from ij.measure import ResultsTable
-from inra.ijpb.label.LabelImages import keepLabels
+from inra.ijpb.label.LabelImages import keepLabels, findAllLabels, remapLabels
+from inra.ijpb.measure.region3d import Centroid3D
 
-up_to = 2
-bottom_first = True
+# How many slices to remove (starting from the most populated ones)
+up_to = 3
+
+# ------------------------------------------------------------------
+
+img = IJ.getImage()
+calib = img.getCalibration()
+c3d = Centroid3D()
 
 # Creating the histogram of spots repartition on the Z axis
-rt    = ResultsTable.getActiveTable()
-vals  = rt.getColumn("Centroid.Z")
-histo = [0 for _ in range(1 + int(math.ceil(max(vals))))]
+d_clb = calib.copy()
+d_clb.pixelWidth  = 1
+d_clb.pixelHeight = 1
+d_clb.pixelDepth  = 1
+d_clb.setUnit("pixel")
 
+all_lbls = findAllLabels(img)
+pts = c3d.analyzeRegions(img.getStack(), all_lbls, d_clb)
+vals = [int(round(p.getZ())) for p in pts]
+
+histo = [0 for _ in range(1 + int(math.ceil(max(vals))))]
 for val in vals:
 	histo[int(round(val))] += 1
 
 # Sorting slices by the number of spots they contain
 sliced = [(v, i) for i, v in enumerate(histo)]
-sliced = list(reversed(sorted(sliced)))
-n_spots, last = sliced[up_to-1]
-last += 1
+sliced = list(sorted(sliced))
+n_spots, last = sliced[-1]
+last += up_to
+print("Removing all spots below slice " + str(last))
 
 to_keep = []
-for l in range(len(vals)):
-	lbl = int(rt.getLabel(l))
-	val = rt.getValue('Centroid.Z', l)
-	if val > last:
+for lbl, pt in zip(all_lbls, pts):
+	if pt.getZ() > last:
 		to_keep.append(lbl)
 
 print("Keeping " + str(len(to_keep)) + " items from " + str(len(vals)))
 
-img = IJ.getImage()
-IJ.run("Select Label(s)", "label(s)="+",".join([str(l) for l in to_keep]))
-
-
-
-
+res_img = keepLabels(img, to_keep)
+remapLabels(res_img)
+res_img.show()
